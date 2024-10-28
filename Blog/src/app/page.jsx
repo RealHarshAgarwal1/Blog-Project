76a16f7ef345a6e4f1ee992b80582d6dd0a82a5c
Harsh Agarwal <Realharshagarwal@gmail.com>
2024-10-28 22:45:57 +0530

    Modifying the UI

--- Blog/src/app/page.jsx
+++ Blog/src/app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return <div className={styles.container}>
   <div className={styles.textContainer}>
     <h1>Creative Thoughts Agency</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aut, delectus fugiat temporibus quaerat voluptates consequuntur doloribus veniam assumenda at?</p>
+    <p>..</p>
     {/* still working...... */}
   </div>
   <div className={styles.buttons}>
