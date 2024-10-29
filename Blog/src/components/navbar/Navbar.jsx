077ae977de68b67106736f281ce791729898d1b5
Harsh Agarwal <Realharshagarwal@gmail.com>
2024-10-29 22:56:18 +0530

    NavBar Modification

--- Blog/src/components/navbar/Navbar.jsx
+++ Blog/src/components/navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
     <Link href="/" className={styles.logo}>Logo</Link>
+    //navbar
     <div>
       <Links/>
     </div>
